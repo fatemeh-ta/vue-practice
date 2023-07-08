f44fa729c2d4c78b33c1094d8530d7e45df300ac
Fatemeh Tashakori <fatemeh.tashakori@live.com>
2023-07-08 09:33:56 +0330

    vue practices

---
+++ main.js
@@ -0,0 +1,12 @@
+new Vue({
+  el: "#app",
+  data: {
+    product: "Socks",
+    image: "./assets/socks-green.jpeg",
+    altText: "alt",
+    description: "A pair of warm, fuzzy socks",
+    inStock: true,
+    onSale: true,
+    link: "https://www.vuemastery.com/courses/intro-to-vue-js/attribute-binding",
+  },
+});
