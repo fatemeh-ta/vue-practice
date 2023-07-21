f1198e96366e2ef2a4861827af9109da14e2e909
Fatemeh Tashakori <fatemeh.tashakori@live.com>
2023-07-21 18:37:04 +0330

    done part1 from practice vue.js

--- main.js
+++ main.js
@@ -8,5 +8,43 @@ new Vue({
     inStock: true,
     onSale: true,
     link: "https://www.vuemastery.com/courses/intro-to-vue-js/attribute-binding",
+    variants: [
+      {
+        varianColor: "green",
+        variantSrc: "./assets/socks-green.jpeg",
+        variantId: 1,
+      },
+
+      {
+        varianColor: "blue",
+        variantSrc: "./assets/socks-blue.jpeg",
+        variantId: 2,
+      },
+    ],
+    sizes: [
+      { key: 0, title: "small" },
+      { key: 1, title: "medium" },
+      { key: 2, title: "large" },
+      { key: 3, title: "xLarge" },
+    ],
+    cart: 0,
+  },
+
+  methods: {
+    addCart() {
+      this.cart += 1;
+    },
+
+    updateImage(image) {
+      this.image = image.variantSrc;
+    },
+
+    decreaseCart() {
+      if (this.cart > 0) {
+        this.cart -= 1;
+      } else {
+        this.cart = 0;
+      }
+    },
   },
 });
